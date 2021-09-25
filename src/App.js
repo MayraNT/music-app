4442ce49d09e50f1896aa9b62869ddbee0ad5ffb
mayrant <mayrant2004@yahoo.com>
2021-09-24 19:53:39 -0500

    added components for login screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import { AppBar } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" component="div">
+              My Music App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <TextField id="standard-basic" label="Username *" variant="standard" />
+      <TextField id="standard-basic" label="Password *" variant="standard" />
+      <Button variant="contained">Login</Button>
     </div>
   );
 }
